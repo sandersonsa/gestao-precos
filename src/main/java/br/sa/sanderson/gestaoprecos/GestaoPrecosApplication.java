99398e8a37adc4fde320c1675af1e216fb6ed35a
sandersonsa <costadesa@gmail.com>
2020-10-17 17:11:05 -0300

    setup inicial

---
+++ src/main/java/br/sa/sanderson/gestaoprecos/GestaoPrecosApplication.java
@@ -0,0 +1,13 @@
+package br.sa.sanderson.gestaoprecos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPrecosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPrecosApplication.class, args);
+	}
+
+}
